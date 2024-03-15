822e513d9066e51f38b7ada1566c099d56d5502e
IsaiahHarvi <harvillerisaiah@gmail.com>
2024-03-15 18:26:54 -0500

    ansi C compliant

--- Programming Assignment 2/harville_mysh2.c
+++ Programming Assignment 2/harville_mysh2.c
@@ -1,6 +1,6 @@
-// Author: Isaiah Harville
-// Date:   2/10/2024
-// To Compile: gcc -ansi -o mysh harville_mysh.c
+/* Author: Isaiah Harville 
+/* Date:   2/10/2024
+/* To Compile: gcc -ansi -o mysh harville_mysh.c */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -14,10 +14,10 @@
 #define MAX_INPUT_SIZE 512
 #define MAX_ARGS 20
 
-// Default Prompt
+/* Default Prompt */
 char prompt[50] = "$ ";
 
-// Function Declarations
+/* Function Declarations */
 void execute_external_command(char **args);
 char *search_in_path(char *path, char *filename);
 void execute_command(char **args);
@@ -32,20 +32,20 @@ void cd_wrapper(char **args);
 void display_help();
 void exit_wrapper();
 
-// Command Strcutre
+/* Command Strcutre */
 typedef struct {
-    char *name; // Command name
+    char *name; /* Command name */
     union {
-        void (*mp_func)(char **args); // multi-parameter function
-        void (*sp_func)(char *arg);   // single-parameter function
-        void (*np_func)();            // no-parameter function
+        void (*mp_func)(char **args); /* multi-parameter function */
+        void (*sp_func)(char *arg);   /* single-parameter function */
+        void (*np_func)();            /* no-parameter function */
     } instruction;
-    int params; // 2 **args, 1 *arg, 0 no args
+    int params; /* 2 **args, 1 *arg, 0 no args */
 } Command;
 
-// List of commands
+/* List of commands */
 Command commands[] = {
-    // {Command Name, {Function}, Number of Parameters}
+    /* {Command Name, {Function}, Number of Parameters} */
     {"echo", {.mp_func = echo_wrapper}, 2}, 
     {"cd", {.mp_func = cd_wrapper}, 2}, 
     {"cat", {.mp_func = cat_wrapper}, 2}, 
@@ -56,7 +56,7 @@ Command commands[] = {
     {"PS1", {.sp_func = change_prompt_style}, 1},
     {"help", {.np_func = display_help}, 0},
     {"exit", {.np_func = exit_wrapper}, 0},
-    {NULL, {NULL}, 0} // End of list
+    {NULL, {NULL}, 0} /* End of list */
 };
 
 int main() {
@@ -64,25 +64,25 @@ int main() {
     char *args[MAX_ARGS];
     char *token;
 
-    while (1 == 1) { // Infinite loop
-        printf("%s", prompt); // Print the prompt
-        fgets(input, MAX_INPUT_SIZE, stdin); // Get the input from the user
+    while (1 == 1) { /* Infinite loop */
+        printf("%s", prompt); /* Print the prompt */
+        fgets(input, MAX_INPUT_SIZE, stdin); /* Get the input from the user */
 
-        size_t length = strlen(input); // Get the length of the input
-        if (input[length - 1] == '\n') { // Remove the newline character
-            input[length - 1] = '\0'; // Replace the newline character with a null terminator
+        size_t length = strlen(input); /* Get the length of the input */
+        if (input[length - 1] == '\n') { /* Remove the newline character */ 
+            input[length - 1] = '\0'; /* Replace the newline character with a null terminator */
         }
 
-        // Parse the input
-        int i = 0;
-        token = strtok(input, " "); // Split the input into tokens
+        /* Parse the input */
+        int i = 0; 
+        token = strtok(input, " "); /* Split the input into tokens */
         while (token != NULL) { 
-            args[i++] = token; // Add the token to the args array
-            token = strtok(NULL, " ");
+            args[i++] = token; /* Add the token to the args array */
+            token = strtok(NULL, " "); 
         } 
-        args[i] = NULL; // Add a null terminator to the end of the args array
+        args[i] = NULL; /* Add a null terminator to the end of the args array */
 
-        // Execution
+        /* Execution */
         execute_command(args);
     }
 }
@@ -92,26 +92,26 @@ void execute_command(char **args) {
         return;
     }
 
-    // Check if the command is one of the built-in commands.
+    /* Check if the command is one of the built-in commands. */
     int i = 0;
     while (commands[i].name != NULL) {
         if (strcmp(commands[i].name, args[0]) == 0) {
             if (commands[i].params == 2) {
-                // Execute multi-parameter function
+                /* Execute multi-parameter function */
                 commands[i].instruction.mp_func(args);
             } else if (commands[i].params == 1) {
-                // Execute single parameter function
+                /* Execute single parameter function */
                 commands[i].instruction.sp_func(args[1]);
             } else {
-                // Execute no parameter function
+                /* Execute no parameter function */
                 commands[i].instruction.np_func();
             }
             return;
         }
-        i++; // Move to the next command
+        i++; /* Move to the next command */
     }
 
-    // If the command is not built-in, attempt to execute it as an external program.
+    /* If the command is not built-in, attempt to execute it as an external program. */
     execute_external_command(args);
 }
 
@@ -121,9 +121,9 @@ void execute_external_command(char **args) {
 
     pid = fork();
     if (pid == 0) {
-        // Child process
+        /* Child process */
         if (strchr(args[0], '/') == NULL) {
-            // Simple program name, search in PATH
+            /* Simple program name, search in PATH */
             char *path = getenv("PATH");
             char *exec_path = search_in_path(path, args[0]);
             if (exec_path != NULL) {
@@ -133,15 +133,15 @@ void execute_external_command(char **args) {
                 printf("Command not found in PATH\n");
             }
         } else {
-            // Absolute or relative path
+            /* Absolute or relative path */
             execv(args[0], args);
         }
-        exit(EXIT_FAILURE); // execv only returns if there is an error
+        exit(EXIT_FAILURE); /* execv only returns if there is an error */
     } else if (pid < 0) {
-        // Error forking
+        /* Error forking */
         perror("fork");
     } else {
-        // Parent process
+        /* Parent process */
         do {
             waitpid(pid, &status, WUNTRACED);
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
@@ -154,47 +154,47 @@ char *search_in_path(char *path, char *filename) {
         char *fullpath = malloc(strlen(token) + strlen(filename) + 2);
         sprintf(fullpath, "%s/%s", token, filename);
         if (access(fullpath, X_OK) == 0) {
-            return fullpath; // Found executable
+            return fullpath; /* Found executable */
         }
         free(fullpath);
         token = strtok(NULL, ":");
     }
-    return NULL; // Not found
+    return NULL; /* Not found */
 }
 
 void echo_wrapper(char **args) {
-    if (args[1] == NULL){ // If empty argument
-        printf("\n"); // Print newline if no arguments
-    } else { // If not empty argument
+    if (args[1] == NULL){ /* If empty argument */
+        printf("\n"); /* Print newline if no arguments */
+    } else { /* If not empty argument */
         int i = 1;
-        if (strcmp(args[1], "-n") == 0) i = 2; // Skip the new line if theres a -n
+        if (strcmp(args[1], "-n") == 0) i = 2; /* Skip the new line if theres a -n */
         while (args[i] != NULL) {
-            printf("%s ", args[i++]); // Print the arguments
+            printf("%s ", args[i++]); /* Print the arguments */
         }
-        if (strcmp(args[1], "-n") != 0) printf("\n"); // Print newline unless -n
+        if (strcmp(args[1], "-n") != 0) printf("\n"); /* Print newline unless -n */
     }
 }
 
 void change_prompt_style(char *newPrompt) {
-    strcpy(prompt, newPrompt); // Copy the new prompt to the prompt variable
-    strcat(prompt, " "); // Add a space after the new prompt for readabiloty
+    strcpy(prompt, newPrompt); /* Copy the new prompt to the prompt variable */
+    strcat(prompt, " "); /* Add a space after the new prompt for readabiloty */
 }
 
 void cat_wrapper(char **args) {
-    FILE *file;  // File pointer
-    char line[MAX_INPUT_SIZE];  // Line buffer
+    FILE *file;  /* File pointer */
+    char line[MAX_INPUT_SIZE];  /* Line buffer */
 
-    // Open the file for reading
+    /* Open the file for reading */
     if ((file = fopen(args[1], "r")) == NULL) { 
         perror("Error opening file !");
         return;
     }
 
-    // Print the contents x`of the file to the terminal
+    /* Print the contents x`of the file to the terminal */
     while (fgets(line, sizeof(line), file) != NULL) {
         printf("%s", line);
     }
-    fclose(file); // Close the file
+    fclose(file); /* Close the file */
 }
 
 void rm_wrapper(char **args) {
@@ -206,20 +206,20 @@ void rm_wrapper(char **args) {
 void cp_wrapper(char **args) {
     FILE *source, *destination;
     int ch;
-    // Check for the correct number of arguments
+    /* Check for the correct number of arguments */
     if (args[1] == NULL || args[2] == NULL) {
-        fprintf(stderr, "Error !\nUsage: cp <source> <destination>\n"); // Print error/usage msg
+        fprintf(stderr, "Error !\nUsage: cp <source> <destination>\n"); /* Print error/usage msg */
         return;
     }
 
-    // Open the source file for reading
+    /* Open the source file for reading */
     source = fopen(args[1], "rb");
     if (source == NULL) {
         perror("Error opening source file");
         return;
     }
 
-    // Open the destination file for writing
+    /* Open the destination file for writing */
     destination = fopen(args[2], "wb");
     if (destination == NULL) {
         perror("Error opening destination file");
@@ -227,18 +227,18 @@ void cp_wrapper(char **args) {
         return;
     }
 
-    // Copy the contents of the source file to the destination file
+    /* Copy the contents of the source file to the destination file */
     while ((ch = fgetc(source)) != EOF) {
-        fputc(ch, destination); // Write the character to the destination file
+        fputc(ch, destination); /* Write the character to the destination file */
     }
 
-    // Close both files
+    /* Close both files */
     fclose(source);
     fclose(destination);
 }
 
 void mkdir_wrapper(char **args) {
-    if (mkdir(args[1], 0777) != 0) { // we dont care about security, freedom for all files
+    if (mkdir(args[1], 0777) != 0) { /* we dont care about security, freedom for all files */
         perror("Error creating directory !");
     }
 }
@@ -250,18 +250,18 @@ void rmdir_wrapper(char **args) {
 }
 
 void cd_wrapper(char **args) {
-    if (args[1] == NULL) { // empty argument
-        fprintf(stderr, "Expected argument to \"cd\"\n"); // print error msg/usage
+    if (args[1] == NULL) { /* empty argument */
+        fprintf(stderr, "Expected argument to \"cd\"\n"); /* print error msg/usage */
     } else {
-        if (chdir(args[1]) != 0) { // change directory
-            perror("cd failed"); // tell user if it failed
+        if (chdir(args[1]) != 0) { /* change directory */
+            perror("cd failed"); /* tell user if it failed */
         } else {
-            change_prompt_style(strcat(args[1], " $")); // include new dir in prompt
+            change_prompt_style(strcat(args[1], " $")); /* include new dir in prompt */
         }
     }
 }
 
-void display_help() { // Display the help message
+void display_help() { /* Display the help message */
     printf("\techo  [optional -n] [args..] - prints the arguments to the terminal with an option to not include a newline\n");
     printf("\tPS1   [new_character]        - changes the prompt msg to the specified chars\n");
     printf("\tcat   [filename]             - prints the contents of the file to the terminal\n");
--- Programming Assignment 2/myls.c
+++ Programming Assignment 2/myls.c
@@ -8,18 +8,18 @@
 #include <sys/wait.h>
 
 void ls_wrapper(char **args) {
-    char command[256] = "ls "; // Create the ls command
-    int i = 1; // Start at the first argument
+    char command[256] = "ls "; /* Create the ls command  */
+    int i = 1; /* Start at the first argument  */
     while (args[i] != NULL) {
-        strcat(command, args[i]); // Add the argument to the command
-        strcat(command, " ");  // Add space between arguments
-        i++; // Move to the next argument
+        strcat(command, args[i]); /* Add the argument to the command  */
+        strcat(command, " ");  /* Add space between arguments  */
+        i++; /* Move to the next argument  */
+    system(command);  /* Execute the ls command  */
     }
-    system(command);  // Execute the ls command
 }
 
 int main(int argc, char *argv[]) {
-    // Pass the command-line arguments to ls_wrapper, skipping the program name
+    /* Pass the command-line arguments to ls_wrapper, skipping the program name  */
     ls_wrapper(argv);
     return 0;
 }
